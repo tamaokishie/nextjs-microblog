7c104dfcd60312f100a1403de826072c4843f933
xie <112142164+tamaokishie@users.noreply.github.com>
2023-06-18 12:15:42 +0900

    画像は表示された

--- components.js/Layout.js
+++ components.js/Layout.js
@@ -1,22 +1,23 @@
+import styles from "./layout.module.css";
 import Head from "next/head";
-import styles from "./Layout.module.css"
-import utilStyles from "../styles/utils.module.css"
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = "Xiexie"
-export const siteTitle = "Next.js blog"
+const name = "Xiexie";
+export const siteTitle = "Next.js Blog";
 
 function Layout({children}) {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<header className={styles.header}>
-				<img src="/images/profile.png" width="200" className={utilStyles.borderCircle}/>
-				<h1 className={utilStyles.heading2Xl}>{name}</h1>
-			</header>
-			<main>{children}</main>
-		</div>
+return (
+	<div className={styles.container}>
+		<Head>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<header className={styles.header}>
+			<img src="/images/profile.png" width="200" className={utilStyles.borderCircle}/>
+			<h1>{name}</h1>
+		</header>
+		<main>{children}</main>
+	</div>
 	);
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
-import Layout from '@/components.js/Layout'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import utilStyle from "../styles/utils.module.css";
+// //import { getSortedPostsData } from "../lib/posts";
+import Layout from "@/components.js/layout";
+
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <Layout>
-      こんにちは！
-    </Layout>
-  )
+	return <Layout>
+		{/* <section className={utilStyle.headingMd}>
+			<p>
+				私はフルスタックエンジニアです
+			</p>
+		</section>
+
+		<section>
+		<h2>エンジニアのブログ</h2>
+		<div className={styles.grid}>
+			<article>
+				<Link href="/">
+					<img src="/images/thumbnail01.jpg" alt=""/>
+				</Link>
+				<Link href="/">
+					<a className={utilStyle.boldText}>
+					SSGとSSRの使い分けの場面はいつなのか？
+					</a>
+				</Link>
+				<br/>
+				<small className={utilStyle.lightText}>February 23, 2023</small>
+			</article>
+		</div>
+		</section> */}
+
+	</Layout>
 }
